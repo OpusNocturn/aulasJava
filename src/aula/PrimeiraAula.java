3083503a745d15cf874c0acb32546276fc70b080
OpusNocturn <gabriel.pimentel.sotc@gmail.com>
2018-08-25 18:09:10 -0300

    upload de aulas javas pelo git

---
+++ src/aula/PrimeiraAula.java
@@ -0,0 +1,16 @@
+package aula;
+
+public class PrimeiraAula {
+	public static void main(String args []) {
+		//variavel do tipo primitivo inteiro
+		int numero = 10, numero2 = 2;
+		/*
+		 * + -> soma
+		 * - -> subtração
+		 * * -> multiplicação
+		 * "/" -> divisão
+		 */
+		System.out.print(numero + numero2);
+	}
+
+}
