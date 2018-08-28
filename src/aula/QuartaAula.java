09021d0ea803c74ee90a42e8f5877fce6920c27f
OpusNocturn <gabriel.pimentel.sotc@gmail.com>
2018-08-27 22:38:35 -0300

    potenciacao adicionada

--- src/aula/QuartaAula.java
+++ src/aula/QuartaAula.java
@@ -11,8 +11,14 @@ public class QuartaAula {
 		return total;
 	}
 	
+	public static int poten(int n1, int n2) {
+		int total = (int) Math.pow( n1, n2);
+		return total;
+	}
+	
 		public static void main(String[] args) {
 		System.out.println(soma(10,2));
 		System.out.println(divisão(10,2));
+		System.out.println(poten(2,2));
 		}
 }
