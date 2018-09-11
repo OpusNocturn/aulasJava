d1370869341d3c0aa8fd03d16cae55180b28181f
Gabrielpdsilva <gabriel.pimentel.sotc@gmail.com>
2018-09-10 22:12:24 -0300

    Aula 6 adicionada

---
+++ src/aula/SextaAula.java
@@ -0,0 +1,44 @@
+package aula;
+
+public class SextaAula {
+	public static void main(String args [])
+	//Exercício 7
+	/*{
+		int r = 12;
+		double p = 3.1415;
+		double v = p*(r*r);
+		System.out.println(v);
+	}*/
+	
+	
+	
+	
+	
+	//Exercício 8
+	/*{
+		int resto = 99 % 4;
+		System.out.println(resto);
+	}*/
+	
+	
+	
+	
+	//Exercício 9
+	/*{
+		int dois = 2;
+		double res = dois * 0.5 ;
+		System.out.println(res);
+	}*/
+	
+	
+	
+	
+	
+	//Exercício 10
+	{
+	int oito = 8;
+	double res = oito**2;
+	System.out.println(res);
+	}
+	
+}
