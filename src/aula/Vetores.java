9113c31d90a0b2c9640e7d94da3e84dc56ff1368
Gabrielpdsilva <gabriel.pimentel.sotc@gmail.com>
2018-10-08 21:59:55 -0300

    exercicio de vetores adicionado

---
+++ src/aula/Vetores.java
@@ -0,0 +1,22 @@
+package aula;
+
+public class Vetores {
+	public static void main(String[] args) {
+		
+		int matriz [][] = null;
+		
+		matriz [0][0] = 1;
+		matriz [0][1] = 10;
+		matriz [0][2] = 20;
+				
+		matriz [1][0] = 2;
+		matriz [1][1] = 20;
+		matriz [1][2] = 40;
+								
+		matriz [2][0] = 3;
+		matriz [2][1] = 52;
+		matriz [2][2] = 74;
+		System.out.println(matriz);
+	}
+
+}
