0769f9ca4339e5db9f6b2bd033b2d95a9cee8ff9
Gabrielpdsilva <gabriel.pimentel.sotc@gmail.com>
2018-10-29 22:06:24 -0300

    mais campos inseridos na janela

--- src/aula/PrimeiraTela.java
+++ src/aula/PrimeiraTela.java
@@ -1,7 +1,6 @@
 package aula;
 
 import java.awt.Container;
-
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
@@ -12,7 +11,7 @@ public class PrimeiraTela extends JFrame{
 	public PrimeiraTela() {
 		super("Titulo da Minha Janela");
 		
-		Container paine = this.getContentPane();      //Fixa os objetos no layout
+		Container paine = this.getContentPane();   //Fixa os objetos no layout
 		
 		paine.add(lblNome);
 		lblNome.setBounds(20,20,100,20);
@@ -20,10 +19,37 @@ public class PrimeiraTela extends JFrame{
 		paine.add(txtNome);
 		txtNome.setBounds(130,20,200,20);
 		
+		////////////////////////////////////////////////////
+		
+		JLabel lblCpf = new JLabel("CPF: ");
+		JTextField txtCpf = new JTextField();
+			
+			
+			paine.add(lblCpf);
+			lblCpf.setBounds(20,50,100,20);
+			
+			paine.add(txtCpf);
+			txtCpf.setBounds(130,50,200,20);
+			
+		////////////////////////////////////////////////////
+			
+		JLabel lblEndereco = new JLabel("Endereço: ");
+		JTextField txtEndereco = new JTextField();
+		
+			paine.add(lblEndereco);
+			lblEndereco.setBounds(20,80,100,20);
+			
+			paine.add(txtEndereco);
+			txtEndereco.setBounds(130,80,200,20);
+		
+		////////////////////////////////////////////////////
+			
 		this.setLayout(null);
 		this.setSize(400, 400);
 		//this.setResizable(0);
 		this.setVisible(true);
+		
+		
 	}
 	public static void main(String args[]) {
 		PrimeiraTela tela = new PrimeiraTela();
