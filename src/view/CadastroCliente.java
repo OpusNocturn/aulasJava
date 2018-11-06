2337c930c251cee36ea98eda05be9237d441b015
Gabrielpdsilva <gabriel.pimentel.sotc@gmail.com>
2018-11-05 22:31:37 -0200

    funcao no botao adicionado

--- src/view/CadastroCliente.java
+++ src/view/CadastroCliente.java
@@ -2,31 +2,45 @@ package view;
 
 import java.awt.Container;
 
+import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
 
 public class CadastroCliente extends JFrame{
 
-	JLabel lblText = new JLabel("Nome: ");
-	JTextField txtNome = new JTextField();
+	JLabel txtNome = new JLabel("Nome: ");
+	JTextField campoNome = new JTextField();
+	JButton btn = new JButton("Clique aqui!");
 	
 public CadastroCliente() {
 	super("Janela"); 				//Nome que aparece na aba
 	
 	Container paine = this.getContentPane();
 	
-	paine.add(lblText);
-	lblText.setBounds(20,20,100,20);
-	
 	paine.add(txtNome);
-	txtNome.setBounds(60,20,150,20);
+	txtNome.setBounds(20,20,100,20);
+	
+	paine.add(campoNome);
+	campoNome.setBounds(60,20,150,20);
+	
+	paine.add(btn);
+	btn.setBounds(140,120,120,40);
+	
+	btn.addActionListener(new ActionListener() {
+		
+		@Override
+		public void actionPerformed(ActionEvent e) {
+			System.out.println("Nome: "+ campoNome.getText());
+		}
+	});
 	
 	this.setLayout(null);
 	this.setVisible(true);
 	this.setResizable(false);
 	this.setLocation(700, 200);
 	this.setSize(400, 200);
+	this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 }
 
 public static void main(String[] args) {
